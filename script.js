f7c82228e975c34de87156d9edbdf50686cee344
chantszying <chantszying@chantszyingdeMacBook-Pro.local>
2020-08-17 19:07:09 +0800

    adding starting project

---
+++ script.js
@@ -0,0 +1,23 @@
+var css = document.querySelector("h3");
+var css2 = document.querySelector("h4");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, "
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+	css2.textContent = "Congrats";
+}
+
+// Add a random button which generates two random numbers for the colour inputs.
+
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
